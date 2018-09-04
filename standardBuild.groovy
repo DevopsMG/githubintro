c96583f64bfbd25669bad45a6932529ff5c49725
maheshgantena <maheshgantena@gmail.com>
2018-09-03 20:43:26 -0400

    Create standardBuild.groovy

---
+++ standardBuild.groovy
@@ -0,0 +1,16 @@
+// See https://github.com/jenkinsci/workflow-cps-global-lib-plugin
+
+// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+// method with the same name as the file.
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    stage 'checkout'
+    node {
+        checkout scm
+        stage 'main'
+        stage 'post'
+    }
+}
